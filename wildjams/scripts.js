c01f779e9d1a494dd0be10013f1a7d97d02b22e2
thomasam3797 <144245879+thomasam3797@users.noreply.github.com>
2023-12-17 20:22:14 -0600

    wj

---
+++ wildjams/scripts.js
@@ -0,0 +1,24 @@
+// gallery component
+document.addEventListener('DOMContentLoaded', function(){
+	var rotate = function(component, step){
+		// figure out width of container
+		var slide = component.querySelector('.slide');
+		let slideWidthVw = ((slide.offsetWidth / document.documentElement.clientWidth)-1) * 23.5;
+		var left = Number(slide.style.left.replace('rem', ''));
+		left += step * 23.5;
+		if(left > 0) left = 0;
+		if(left < -slideWidthVw) left = -slideWidthVw;
+		slide.style.left = left+'rem';
+	};
+
+	var components = document.querySelectorAll('.gallery');
+	for(var i = 0; i < components.length; i++){
+		var component = components[i];
+		component.querySelector('.left').addEventListener('click', function(){
+			rotate(component, 1);
+		});
+		component.querySelector('.right').addEventListener('click', function(){
+			rotate(component, -1);
+		});
+	}
+});
